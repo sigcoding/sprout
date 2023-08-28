6da93b84c0586d2c9919518b687b861a850e93de
Victor Ribeiro Boechat <victor2005contato@gmail.com>
2023-08-28 12:50:06 -0300

    feat: add divider argTypes description

--- src/components/Divider/Divider.stories.ts
+++ src/components/Divider/Divider.stories.ts
@@ -14,6 +14,7 @@ const meta = {
   },
   argTypes: {
     direction: {
+      description: "Divider direction, horizontal or vertical",
       options: ["horizontal", "vertical"],
       control: {
         type: "inline-radio",
